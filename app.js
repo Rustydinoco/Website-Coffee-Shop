76ed63f10a8f1dba229c3ff1f8d146ea5da6a905
Bilal Achmad Ramadhan <bilaldoank04@gmail.com>
2023-11-20 20:27:17 +0700

    adding Hamburger Sidebar

---
+++ app.js
@@ -0,0 +1,11 @@
+const navbarNav = document.querySelector('.navbar-nav')
+const hamburger = document.querySelector('#hamburger')
+
+hamburger.addEventListener ('click',() => { 
+    navbarNav.classList.toggle('active')
+})
+
+document.addEventListener('click',(e)=>{
+    if(!hamburger.contains(e.target)&&!navbarNav.contains(e.target))
+    navbarNav.classList.remove('active')
+})
