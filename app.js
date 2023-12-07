5db8d017442f42caed3a62a0e942d3f89f16a126
Bilal Achmad Ramadhan <bilaldoank04@gmail.com>
2023-12-07 21:51:01 +0700

    adding search box

--- app.js
+++ app.js
@@ -5,7 +5,25 @@ hamburger.addEventListener ('click',() => {
     navbarNav.classList.toggle('active')
 })
 
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#searchBox')
+const searchButton = document.querySelector('#searchButton')
+
+searchButton.addEventListener ('click',(e) => { 
+    searchForm.classList.toggle('active')
+    searchBox.focus()
+    e.preventDefault()
+})
+
+const hm = document.querySelector('#hamburger')
+const sb = document.querySelector('#searchButton')
+
 document.addEventListener('click',(e)=>{
-    if(!hamburger.contains(e.target)&&!navbarNav.contains(e.target))
+    if(!hm.contains(e.target)&&!navbarNav.contains(e.target))
     navbarNav.classList.remove('active')
 })
+
+// document.addEventListener('click',(e)=>{
+//     if(!sb.contains(e.target)&&!searchForm.contains(e.target))
+//     searchForm.classList.remove('active')
+// })
